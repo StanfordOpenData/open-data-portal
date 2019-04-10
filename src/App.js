103c67ac70e15b92f57ec4f174a14ed5fe8ff4dd
Ashwin Ramaswami <aramaswamis@gmail.com>
2019-04-10 11:12:59 -0700

    feat: add logo and simplify component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+export default () => (
+  <div className="App">
+    <header className="App-header">
+      <img className="App-logo" src="https://user-images.githubusercontent.com/1689183/55673023-25239a00-5857-11e9-9699-5f2d0ab365cf.png" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
           </a>
-        </header>
-      </div>
-    );
-  }
-}
-
-export default App;
+    </header>
+  </div>
+);
