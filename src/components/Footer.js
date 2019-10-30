99fcc0172fafe66b32a7a3fc409121fdc0080b6e
Grace <gkzhou@stanford.edu>
2019-10-30 16:12:20 -0700

    Revert "Test"
    This reverts commit 252d674a3bde9b01dece1bd1767676bdf1ce720f, reversing
    changes made to 207b4e682aa12bf8ba2cd6730ba4c11c3c8f674c.

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+function Footer() {
+    return (
+      <div className="footer">
+        © 2019 The Stanford Daily Publishing Corporation.
+      </div>
+    )
+  }
+
+export default Footer;
