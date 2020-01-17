85f92c9e4a3da53079325e04fc01b4bc02bdc65e
Emily Wen <emilywen2000@gmail.com>
2020-01-17 11:02:13 -0800

    fix top bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import './styles.css';
 import { Link } from "react-router-dom";
+import logoImage from './logo.jpg';
 
 function NavBar() {
   return (
     <div>
         <nav>
-          <Link to="/" className="logo"></Link>
+          <Link to="/" className="logo"><img src={logoImage} alt="logo" style= {{verticalAlign: 'middle', height:'3em'}}/></Link>
           <ul>
             <li className="leftNav"><Link to="/jobs">Datasets</Link></li>
             {/* <li><a href="/">Email alerts</a></li> */}
             <li><Link to="/advice">About Us</Link></li>
             <li><Link to="/post" className="btnSecondary">Contribute a dataset</Link></li>
-            <li><Link to="https://stanforddaily.com/" target="_blank"> Stanford Daily</Link></li>
+            <li><a href="https://stanforddaily.com/" target="_blank"> Stanford Daily</a></li>
           </ul>
         </nav>
     </div>
