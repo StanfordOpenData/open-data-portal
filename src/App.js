6056bfbf42afcb1aef2e25e11a5f283e66f5e272
Arjun Ramani <arjun@Arjuns-MacBook-Pro-2.local>
2020-03-17 23:33:06 -0400

    react helmet

--- src/App.js
+++ src/App.js
@@ -11,28 +11,45 @@ import AboutUs from './components/AboutUs';
 import Footer from './components/Footer';
 //import Payment from './components/Payment';
 import NotFound from './components/NotFound';
+import {Helmet} from 'react-helmet';
 
 class App extends Component {
   render() {
     return (
-      <div className="flexWrapper">
-      <div className="wrapper">
-        <HashRouter>
-          <div>
-            <NavBar />
-            <Switch>
-              <Route exact path='/' component={LandingPage} />
-              <Route exact path='/datasets' component={Datasets} />
-              <Route path='/datasets/:name' component={DatasetDetails} />
-              <Route exact path='/aboutus' component={AboutUs} />
-              <Route exact path='/contribute' component={PostDataset} />
-              <Route path="*" component={NotFound} />
-            </Switch>
-          </div>
-        </HashRouter>
-        
-      </div>
-      <Footer />
+      <div className="application">
+        <Helmet>
+          <title>Stanford Open Data Portal</title>
+          <meta name="description" content="Check out our datasets and contribute your own!"/>
+          {/* meta tags for facebook  */}
+          <meta property="og:title" content="Stanford Open Data Portal"/>
+          <meta property="og:description" content="Check out our datasets and contribute your own!"/>
+          <meta property="og:image" content="logo.jpg"/>
+          <meta property="og:url" content="http://opendata.stanforddaily.com"/>
+          {/* <!-- meta tags for twitter --> */}
+          <meta name="twitter:title" content="Stanford Open Data Portal"/>
+          <meta name="twitter:description" content="Check out our datasets and contribute your own!"/>
+          <meta name="twitter:image" content="logo.jpg"/>
+          <meta name="twitter:card" content="summary"/>
+        </Helmet>
+        <div className="flexWrapper">
+        <div className="wrapper">
+          <HashRouter>
+            <div>
+              <NavBar />
+              <Switch>
+                <Route exact path='/' component={LandingPage} />
+                <Route exact path='/datasets' component={Datasets} />
+                <Route path='/datasets/:name' component={DatasetDetails} />
+                <Route exact path='/aboutus' component={AboutUs} />
+                <Route exact path='/contribute' component={PostDataset} />
+                <Route path="*" component={NotFound} />
+              </Switch>
+            </div>
+          </HashRouter>
+          
+        </div>
+        <Footer />
+        </div>
       </div>
     );
   }
