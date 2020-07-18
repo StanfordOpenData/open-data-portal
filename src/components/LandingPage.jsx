53a3b211eb97cd3726d0af4790ee387b7f6633a9
Emily Wen <emilywen2000@gmail.com>
2020-07-18 10:09:49 -0400

    try to unbreak

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './styles.css';
-import { Link, Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import heroImage from './static/heroImage.svg';
 import Stanford from './static/StanfordOval@2x.png'
 import axios from 'axios';
