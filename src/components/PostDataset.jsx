380070de1763ec756833f11918e5e979d480659c
Emily Wen <emilywen2000@gmail.com>
2020-07-30 23:14:58 -0400

    embed google form for dataset collection (#51)
    * embed google form
    
    * fix lint

--- src/components/PostDataset.jsx
+++ src/components/PostDataset.jsx
@@ -67,8 +67,14 @@ class PostDataset extends React.Component{
 		return(
 			<div className = "container">
 				<h2> Contribute a Dataset! </h2>
-				<p> Feel free to upload any Stanford-related datasets you have to the folder below, or you can send us a link via the form below. </p>
-				<iframe title="Upload dataset to Box folder" src="https://stanford.app.box.com/upload-widget/view/9v0zhgp52kv2km032ec3v4ede1y7nxfx/102172673701" width="500" height="600" frameborder="0" allowfullscreen webkitallowfullscreen msallowfullscreen></iframe>
+				<p>
+          Have a dataset to contribute? If it is publicly available or you have
+          permission to release it publicly, we'd love to have you send it to
+          us! If you're a Stanford affiliate, use the Google form below. If not,
+          use the contact us form at the bottom of this page to send us an email.
+        </p>
+				<iframe title="datasetCollectionForm" id="datasetCollectionForm" src="https://docs.google.com/forms/d/e/1FAIpQLSdM46yW_w-EJdkeasODKo5WfaHnQRKoGrnFqkNNb67unDEocg/viewform?embedded=true" width="640" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+				<hr/>
 				<h2> Contact Us! </h2>
 				<p>In the form below, you can leave us a message or drop 
 					us a link to an interesting Stanford dataset you found (or would like to see)! </p>
