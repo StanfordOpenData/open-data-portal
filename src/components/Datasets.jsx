6ee2328c4a72ff4749163b6d0331a60eb2eb200f
vannstrasen <vannstrasen@gmail.com>
2020-10-31 14:23:26 -0500

    small changes to urls

--- src/components/Datasets.jsx
+++ src/components/Datasets.jsx
@@ -86,7 +86,7 @@ class Datasets extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('https://ucopendata.s3.us-east-2.amazonaws.com/metadata_stanford.json')
+    axios.get('https://ucopendata.s3.us-east-2.amazonaws.com/metadata_uchicago.json')
       .then(result => {
         this.setState({
           isLoaded: true,
